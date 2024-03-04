d492f200e6f27d0535f23bc4ec15bf879f50360e
shrikantsurya1c <shrikantsurya.github@gmail.com>
2024-03-04 23:58:28 +0530

    second commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,9 +52,7 @@ android {
 
 dependencies {
     // Compose Intent(Navigation)
-    val nav_version = "2.7.7"
-
-    implementation("androidx.navigation:navigation-compose:$nav_version")
+    implementation ("androidx.navigation:navigation-compose:2.7.7")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     val lifecycle_version = "2.7.0"
@@ -71,6 +69,9 @@ dependencies {
     // Image Loading
     implementation("io.coil-kt:coil-compose:2.4.0")
 
+    // Splash Screen
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -86,4 +87,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
 }
--- app/src/main/java/com/surya/recipeapp/MainActivity.kt
+++ app/src/main/java/com/surya/recipeapp/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.surya.recipeapp.ui.theme.RecipeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             val navController = rememberNavController()
             RecipeAppTheme {
